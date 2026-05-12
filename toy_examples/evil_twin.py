48812c75437fa26787eb08100bbadcc4c4f761f4
Ohad Sheelo <Ohadshee@post.bgu.ac.il>
2026-05-12 16:03:42 +0300

    Before GPT changes to evil twin

--- toy_examples/evil_twin.py
+++ toy_examples/evil_twin.py
@@ -17,24 +17,54 @@ def evil_twin_example(rng,n,p):
     This should yield a high unique information for both X and Y, and zero shared information.
     """
     #Twin sonic (Unique for both)
-    R_s = rng.standard_normal((n, p))
-    R_s *= (0.5 / p) ** 0.5 #Var(R_s) = 0.5
+    R_so = rng.standard_normal((n, p))
+    R_so *= (0.5 / p) ** 0.5 #Var(R_so) = 0.5
 
     N = rng.standard_normal((n, p))
-    N *= (0.5 / p) ** 0.5 #Var(N) = 0.5
+    N *= (2.5 / p) ** 0.5 #Var(N) = 2.5
 
     N_t = rng.standard_normal((n, p))
 
-    U1 = rng.standard_normal((n, p))
+    U1_so = rng.standard_normal((n, p))
+    U1_so *= (2.5 / p) ** 0.5 #Var(U1_so) = 2.5
 
-    U2 = rng.standard_normal((n, p))
+    U2_so = rng.standard_normal((n, p))
+    U2_so *= (0.5 / p) ** 0.5 #Var(U2_so) = 0.5
 
-    X1 = R_s + N + U1
-    X2 = R_s + N + U2
+    X1_so = R_so + N + U1_so
+    X2_so = R_so + N + U2_so
 
-    T = R_s + U1 + U2 + N_t
+    T_so = R_so + U1_so + U2_so + N_t
 
 
 
     # Twin Shadow (No unique for X2)
+    R_sh = rng.standard_normal((n, p))
+    R_sh *= (1 / p) ** 0.5 #Var(R_sh) = 1
+    
+    N_sh = rng.standard_normal((n, p))
+    N_sh *= (2 / p) ** 0.5 #Var(N_sh) = 2
+    E1_sh = rng.standard_normal((n, p))
+    E1_sh *= (0.5 / p) ** 0.5
+
+    E2_sh = rng.standard_normal((n, p))
+    E2_sh *= (0.5 / p) ** 0.5
+
+    U1_sh = rng.standard_normal((n, p))
+    U1_sh *= (2 / p) ** 0.5 #Var(U1_sh) = 2
+
+    N_t_sh = rng.standard_normal((n, p))
+
+    X1_sh = R_sh + N_sh + U1_sh + E1_sh
+    X2_sh = R_sh + N_sh + E2_sh
+    T_sh = R_sh + U1_sh + N_t_sh
+
+    return {'sonic': (X1_so, X2_so, T_so), 'shadow': (X1_sh, X2_sh, T_sh)}
+
+
+def main():
+    rng = np.random.default_rng(0)
+    n = 10000
+    p = 10
+    data = evil_twin_example(rng, n, p)
     
