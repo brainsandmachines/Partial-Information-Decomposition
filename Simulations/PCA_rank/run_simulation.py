b447809f1c53fb4ad220403b8902c3d82f51d717
ohadshee <ohadshee@slurm.bgu.ac.il>
2026-07-06 23:31:35 +0300

    Fixed running simulation for pca CV method comperasion

--- Simulations/PCA_rank/run_simulation.py
+++ Simulations/PCA_rank/run_simulation.py
@@ -27,14 +27,14 @@ def run_simulation(
     X = data["X"]
 
     # Run eigenvector PCA with cross-validation
-    max_components = min(n_samples, n_features) - 1
+    max_components = min(n_samples, n_features) - 2
 
 
     # EM-wold
     start = perf_counter()
     ncp_estimate = estimate_ncp_pca(X,ncp_max = max_components ,method="EM",method_cv = 'Kfold' ,nbsim=5,seed  = random_state)
     end_ncp =  perf_counter()
-    ncp_estimate['time'] = end_ncp- start 
+    ncp_estimate['time'] = end_ncp - start 
     
     
     eigenvector_result  = eigenvector_pca_cv(
@@ -64,31 +64,70 @@ def run_simulation(
 
 if __name__ == "__main__":
     # Example usage
-    n_samples = 100
-    n_features = 50
-    rank = 10
-    loading_corr = 0.4
-    noise_std = 0.3
+    n_samples = [100]
+    n_features = [50,70,80]
+    rank = [5,10,20,40]
+    loading_corr = [0.1,0.5,0.9]
+    noise_std = [0.1,0.3,0.9,1.2]
     random_state = 56 
-    results = run_simulation(
-        n_samples=n_samples,
-        n_features=n_features,
-        rank=rank,
-        loading_corr=loading_corr,
-        noise_std=noise_std,
-        random_state=random_state,
-    )
-    
-    eigenvector_result = results["eigenvector_result"]
-    rowwise_result = results["rowwise_result"]
-    ncp_estimate = results["ncp_estimate"]
+    total_runs = len(n_samples) * len(n_features) * len(rank) * len(loading_corr) * len(noise_std)
+    run_count = 0
+    for n_s in n_samples:
+        for n_f in n_features:
+            for r in rank:
+                for l_c in loading_corr:
+                    for n_s_d in noise_std:
+                        print(f"="*70)
+                        print(f"\nRunning simulation with n_samples={n_s}, n_features={n_f}, rank={r}, loading_corr={l_c}, noise_std={n_s_d}")
+                        print(f"\nTRUE RANK: {r}")
+                        results = run_simulation(
+                            n_samples=n_s,
+                            n_features=n_f,
+                            rank=r,
+                            loading_corr=l_c,
+                            noise_std=n_s_d,
+                            random_state=random_state,
+                        )
+                        
+                        eigenvector_result = results["eigenvector_result"]
+                        rowwise_result = results["rowwise_result"]
+                        ncp_estimate = results["ncp_estimate"]
 
-    print("\nEigenvector PCA selected components:", eigenvector_result.selected_n_components, "\n It took", eigenvector_result.time, "seconds to run")
-    
+                        print("\nEigenvector PCA selected components:", eigenvector_result.selected_n_components, "\n It took", eigenvector_result.time, "seconds to run")
+                        
 
 
-    print("\nEM-wold estimated components:", ncp_estimate['ncp'], "\n It took", ncp_estimate['time'], "seconds to run")
+                        print("\nEM-wold estimated components:", ncp_estimate['ncp'], "\n It took", ncp_estimate['time'], "seconds to run")
 
 
-    print("\nRow-wise LOOCV PCA selected components mean over fold:", np.mean(np.array(rowwise_result.n_components_per_fold)),"\n It took", rowwise_result.time, "seconds to run")
-    print('\n Row-wise LOOCV PCA selected components minimum fold:', min(rowwise_result.n_components_per_fold))
+                        print("\nRow-wise LOOCV PCA selected components mean over fold:", np.mean(np.array(rowwise_result.n_components_per_fold)),"\n It took", rowwise_result.time, "seconds to run")
+                      
+                      
+                        print('\n Row-wise LOOCV PCA selected components minimum fold:', min(rowwise_result.n_components_per_fold))
+                        run_count += 1
+                      
+                        print(f"Completed {run_count}/{total_runs} simulations.")
+
+
+# results = run_simulation(
+#     n_samples=n_samples,
+#     n_features=n_features,
+#     rank=rank,
+#     loading_corr=loading_corr,
+#     noise_std=noise_std,
+#     random_state=random_state,
+# )
+
+# eigenvector_result = results["eigenvector_result"]
+# rowwise_result = results["rowwise_result"]
+# ncp_estimate = results["ncp_estimate"]
+
+# print("\nEigenvector PCA selected components:", eigenvector_result.selected_n_components, "\n It took", eigenvector_result.time, "seconds to run")
+
+
+
+# print("\nEM-wold estimated components:", ncp_estimate['ncp'], "\n It took", ncp_estimate['time'], "seconds to run")
+
+
+# print("\nRow-wise LOOCV PCA selected components mean over fold:", np.mean(np.array(rowwise_result.n_components_per_fold)),"\n It took", rowwise_result.time, "seconds to run")
+# print('\n Row-wise LOOCV PCA selected components minimum fold:', min(rowwise_result.n_components_per_fold))
